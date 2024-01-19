5f47005ff1001f40371c3abac204598bbd41269f
TechnoDraconic <riandoty@gmail.com>
2024-01-19 07:41:19 -0800

    haha it works

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,16 +1,19 @@
-import { ReactNode, useSyncExternalStore } from "react"
-import floor from "./assets/floor.svg"
+import { FormEventHandler, useState, useSyncExternalStore } from "react"
+import floorSvg from "./assets/floor.svg"
+import boxSvg from "./assets/box.svg"
+import routerSvg from './assets/router.svg'
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
-import * as THREE from "three"
-import { Floor, Model, Room } from "./editor-classes"
+import { Floor, Model, Node, Room } from "./editor-classes"
 
 const myModel = new Model()
-const myFloor = new Floor(myModel, 0)
-console.log(myFloor.model)
-const myRoom1 = new Room(myFloor)
+const myFloor = new Floor()
+const myRoom1 = new Room()
+myModel.add(myFloor)
+myFloor.add(myRoom1)
 
-function Workspace() {
+function Workspace({ model }: { model: Model }) {
+
     return (
         <Canvas>
             <directionalLight
@@ -24,43 +27,121 @@ function Workspace() {
                 intensity={Math.PI / 2}
             />
             <OrbitControls />
-            {myModel.render()}
+            {model.render()}
         </Canvas>
     )
 }
 
-function TopBar() {
-    const onClick = () => {
-        new Floor(myModel,0)
-        console.log(myModel.tree())
+function JSONConverter({ model }: { model: Model }) {
+    const [JSONInput, setJSONInput] = useState('')
+    const [JSONInputError, setJSONInputError] = useState('')
+    const [JSONOutput, setJSONOutput] = useState('')
+
+    const toJSON = () => {
+        const converted = JSON.stringify(model.toJSON())
+        console.log(converted)
+        setJSONOutput(converted)
+    }
+
+    const fromJSON: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault()
+
+        try {
+            const myJSON = JSON.parse(JSONInput)
+            myModel.replace(Node.fromJSON(myJSON))
+            setJSONInputError('')
+        } catch (e) {
+            setJSONInputError(`Invalid JSON: ${e}`)
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={fromJSON} autoComplete="off">
+                <input type="text" name="JSON Input" value={JSONInput} onChange={(e) => setJSONInput(e.target.value)} />
+                <input type="submit" value={'From JSON (loses data!)'} />
+                {JSONInputError.length ? <div>{JSONInputError}</div> : null}
+            </form>
+            <div>
+                <button onClick={toJSON}>Convert Workspace to JSON</button>
+                <textarea value={JSONOutput} readOnly />
+            </div>
+        </div>
+    )
+}
+
+function TopBar({ model }: { model: Model }) {
+    const onFloorClick = () => {
+        new Floor({ height: 1 }).addTo(model.source)
+        console.log(model.source.tree())
+    }
+
+    const onRoomClick = () => {
+        let parent: Node
+        if (model.source.selectionManager.selected?.name === 'Floor') {
+            parent = model.source.selectionManager.selected.source
+        } else {
+            parent = model.source.findFirst('Floor') as Floor
+        }
+        new Room({ position: [2, 0, 2] }).addTo(parent)
+    }
+
+    const onRouterClick = () => {
+        model.source.optimizationManager.optimize()
     }
 
     return (
         <div className="top-bar">
-            <button id="floor-button" onClick={onClick}>
-                <img src={floor} />
+            <button id="floor-button" onClick={onFloorClick}>
+                <img src={floorSvg} />
+            </button>
+            <button id="room-button" onClick={onRoomClick}>
+                <img src={boxSvg} />
+            </button>
+            <button id="router-button" onClick={onRouterClick}>
+                <img src={routerSvg} />
             </button>
         </div>
     )
 }
 
-function DocumentTree() {
-    const model = useSyncExternalStore(myModel.subscribe, myModel.getSnapshot)
-    console.log('DocumentTree Updated!')
+function DocumentTree({ model }: { model: Model }) {
+    const selectionManager = model.source.selectionManager
+
+    function NodeButton({ node }: { node: Node }) {
+        const selected = selectionManager.isSelected(node)
+        return (
+            <button className={selected ? 'selected' : ''} onClick={() => selectionManager.select(node)}>{node.constructor.name}</button>
+        )
+    }
+
+    function generateButtons(node: Node) {
+        const children = Array.from(node.children)
+        return (
+            <li>
+                <NodeButton node={node} />
+                {children.length ? <ul>{children.map(generateButtons)}</ul> : null}
+            </li>
+        )
+    }
+
     return (
-        <p className="document-tree">{model.tree()}</p>
+        <ul className="document-tree">
+            {generateButtons(model)}
+        </ul>
     )
 }
 
 function Editor() {
-
+    const model = useSyncExternalStore(myModel.subscribe, myModel.getSnapshot)
 
     return (
         <div className="editor">
-            <TopBar />
+            <TopBar model={model} />
             <div className="canvas-area">
-                <DocumentTree />
-                <Workspace />
+                <DocumentTree model={model} />
+                <Workspace model={model} />
+                <JSONConverter model={model} />
             </div>
         </div>
     )
--- src/editor-classes.tsx
+++ src/editor-classes.tsx
@@ -1,40 +1,123 @@
-import { Object3DNode } from "@react-three/fiber"
-import { Children } from "react"
+import { Point, Points } from "@react-three/drei"
+import { ThreeElements } from "@react-three/fiber"
+import { ReactNode } from "react"
 import * as THREE from "three"
 
-export class Node {
-    parent: Node | null
-    children: Set<Node>
+type Constructor = { new(...args: any[]): any }
 
-    constructor(parent: Node | null = null) {
-        this.parent = parent
-        this.children = new Set()
-        if (parent) parent.add(this)
+type JSONTree = { name: string, args: { [key: string]: unknown }, children: JSONTree[] }
+
+type PointCloud = [THREE.Vector3, Router|null][]
+
+export class Node {
+    parent?: Node
+    key: string
+    selected: boolean
+    source = this // Points to the actual node in the case of snapshots
+    name = 'Node'
+    children = new Set<Node>()
+
+    constructor({ key = String(Math.floor(Math.random() * 1000000000)) }: { key?: string } = {}) {
+        this.key = key
+        this.selected = false
     }
 
-    add(node: Node) {
+    add(node: Node, silent = false) {
         this.children.add(node)
         node.parent = this
+        if (!silent) this.onHeirarchyChange()
     }
 
-    remove(node: Node) {
+    remove(node: Node, silent = false) {
         this.children.delete(node)
-        node.parent = null
+        delete node.parent
+        if (!silent) this.onHeirarchyChange()
     }
 
-    addTo(node: Node) {
-        node.add(this)
+    deleteSelf(silent = false) {
+        this.parent?.remove(this)
+        if (this.parent && !silent) this.onHeirarchyChange()
+    }
+
+    addTo(node: Node, silent = false) {
+        node.add(this, silent)
         return this
+    }
+
+    findFirst(name: string) {
+        return this.getDescendants(true).find(n => n.name === name)
+    }
+
+    getDescendants(includeThis = false): Node[] {
+        const out = Array.from(this.children).map(c => c.getDescendants(true)).flat()
+        if (includeThis) out.push(this)
+        return out
+    }
+
+    onHeirarchyChange() {
+        if (this.source !== this && !this.parent) console.warn('Snapshots should not be changed! Use Node.source to change the actual heirarchy instead.')
+        this.parent?.onHeirarchyChange()
+    }
+
+    clone(): this {
+        const out: this = new (this.constructor as Constructor)(this.getArgs())
+        out.selected = this.selected
+        return out
+    }
+
+    generateSnapshot(): this {
+        const out = this.clone()
+        out.source = this
+        this.children.forEach(c => {
+            out.add(c.generateSnapshot(), true)
+        })
+        return out
+    }
+
+    render(): ReactNode {
+        return <>{Array.from(this.children).map(c => c.render())}</>
+    }
+
+    getArgs(): { [key: string]: unknown } {
+        return { key: this.key }
+    }
+
+    toJSON(): JSONTree {
+        // Throw a warning if a non-Node class has its name set as Node,
+        // because that's probably just me being a dumbass.
+        if (this.name !== this.constructor.name && this.name === 'Node') console.warn(`Class ${this.constructor.name} has property "name" set as ${this.name}. Was the name properly set?`)
+
+        const out = { name: this.name, args: this.getArgs(), children: Array.from(this.children).map(c => c.toJSON()) }
+
+        return out
+    }
+
+    static fromJSON(json: JSONTree): Node {
+        const out = fromJSONDictionary[json.name](json.args)
+
+        json.children.forEach(c => {
+            out.add(this.fromJSON(c))
+        })
+
+        return out
+    }
+
+    //Keeps the node this is called on, replaces children.
+    replace(node: Node) {
+        this.children.forEach(c => this.remove(c, true))
+        node.children.forEach(c => this.add(c, true))
+
+        this.onHeirarchyChange()
     }
 
     /*
     Returns the entire tree under this node as a string
 
     Model
-     ⌞Floor1
-      ⌞Room1
-       ⌞Router1
-      ⌞Room2
+    ⌞_Floor1
+    ⌞__Room1
+    ⌞___Router1
+    ⌞__Room2
     etc.
     */
     tree() {
@@ -44,7 +127,7 @@ export class Node {
         let out = className(this)
         function addChildrenToOut(node: Node, padding = 1) {
             node.children.forEach(n => {
-                out += `\n${' '.repeat(padding)}⌞${className(n)}`
+                out += `\n⌞${'_'.repeat(padding)}${className(n)}`
                 addChildrenToOut(n, padding + 1)
             })
         }
@@ -54,142 +137,299 @@ export class Node {
     }
 }
 
-export class ModelNode extends Node {
-    model!: Model
-    declare children: Set<ModelNode>
-
-    constructor(parent: Node | null = null) {
-        super(parent)
-    }
-
-    add(node: ModelNode) {
-        super.add(node)
-        this.model.addToModel(node)
-    }
-
-    remove(node: ModelNode) {
-        super.remove(node)
-        this.model.onModelChange()
-    }
-
-    delete() {
-        this.parent?.remove(this)
-    }
-
-    generateSnapshot(parent: Node) {
-        return new ModelNode(parent)
-    }
-
-    render() {
-        return <></>
-    }
-}
-
-type Snapshot = Node
 export class Model extends Node {
-    declare children: Set<ModelNode>
+    declare children: Set<Node>
     listeners: Set<() => void>
-    snapshot!: Snapshot
-    isSnapshot: boolean
+    snapshot!: Model
+    selectionManager: SelectionManager
+    optimizationManager: OptimizationManager
+    name = 'Model'
 
-    constructor(isSnapshot = false) {
-        super()
+    constructor({ key }: { key?: string } = {}) {
+        super({ key })
         this.listeners = new Set();
+        this.selectionManager = new SelectionManager(this)
+        this.optimizationManager = new OptimizationManager(this)
 
         this.subscribe = this.subscribe.bind(this)
         this.getSnapshot = this.getSnapshot.bind(this)
-
-        this.isSnapshot = isSnapshot
-    }
-
-    add(node: ModelNode) {
-        super.add(node)
-        node.model = this
-        if (!this.isSnapshot && node.children) this.onModelChange()
     }
 
     subscribe(listener: () => void) {
+        console.log('Subscribed!')
         this.listeners.add(listener)
-        return () => this.listeners.delete(listener)
-    }
-
-    onModelChange() {
-        if (!this.isSnapshot) {
-            console.log('Pinging ')
-            this.generateSnapshot()
-            this.listeners.forEach(l => l())
+        return () => {
+            console.log('Unsubscribed!')
+            this.listeners.delete(listener)
         }
     }
 
+    onHeirarchyChange() {
+        super.onHeirarchyChange() // For completeness, even if there is never a parent
+        this.optimizationManager.onHeirarchyChange()
+        this.reRender()
+    }
+
+    onSelectionChange() {
+        this.reRender()
+    }
+
+    reRender() {
+        console.log('Model re-rendering')
+        this.generateSnapshot()
+        this.listeners.forEach(l => l())
+    }
+
     generateSnapshot() {
-        const out = new Model(true)
-        this.children.forEach(c => {
-            c.generateSnapshot(out)
-        })
-        this.snapshot = out
-        return out
+        this.snapshot = super.generateSnapshot()
+        return this.snapshot as this
     }
 
     getSnapshot() {
         return this.snapshot || this.generateSnapshot()
     }
+}
 
-    addToModel(node: ModelNode) {
-        node.model = this
-        this.onModelChange()
+// Manages which node is currently selected
+export class SelectionManager {
+    nodes: Set<Node>
+    model: Model
+    selected: Node | null
+
+    constructor(model: Model) {
+        this.model = model
+        this.nodes = new Set()
+        this.selected = null
     }
 
-    render() {
-        return <>{Array.from(this.children).map(c => c.render())}</>
+    select(node: Node) {
+        if (node.source !== node) node = node.source
+
+        console.log(`${node.constructor.name} selected!`)
+        if (this.selected !== null) {
+            this.selected.selected = false
+        }
+        this.selected = node
+        node.selected = true
+
+        this.model.onSelectionChange()
+    }
+
+    unselect() {
+        this.selected = null
+    }
+
+    isSelected(node: Node) {
+        return node === this.selected || node.key === this.selected?.key
+    }
+
+    refreshList() {
+        this.nodes = new Set(this.model.getDescendants(true))
     }
 }
 
-export class Floor extends ModelNode {
-    height: number
+// The big hochi mama. Implements the k-means clustering algoritm for routers
+export class OptimizationManager {
+    model: Model
+    rooms = new Set<Room>()
+    routers = new Set<Router>()
 
-    constructor(parent: Model, height: number = 0) {
-        super(parent)
-        this.model = parent
+    constructor(model: Model) {
+        this.model = model
+    }
+
+    onHeirarchyChange() {
+        this.rooms = new Set(this.model.getDescendants(true).filter(n => n.name === 'Room') as Room[])
+    }
+
+    optimize(routerCount?: number) {
+        const rooms = Array.from(this.rooms)
+        // The list of points that are to be optimized towards.
+        const observations: PointCloud = rooms.flatMap(r => r.getPointCloud()).map(o => [o, null])
+        if (typeof routerCount !== 'number' || routerCount > rooms.length) routerCount = rooms.length
+
+
+        console.log(`Optimizing for ${routerCount} routers!`)
+        //Dispose of old routers
+        this.routers.forEach(r => r.deleteSelf())
+
+        //Generating new routers
+        this.routers = new Set<Router>()
+        for (let i = 0; i < routerCount; i++) {
+            this.routers.add(new Router({ position: rooms[i].position.clone().toArray() }).addTo(this.model))
+        }
+
+        let iter = 0
+        let reassignments = 0
+        const assign = () => {
+            observations.forEach((observation) => {
+                const [o, router] = observation
+                const closest = Array.from(this.routers).reduce((best, current) => {
+                    if (current.position.distanceTo(o) < best.position.distanceTo(o)) {
+                        return current
+                    }
+                    return best
+                })
+                if (router !== closest) {
+                    router?.points.delete(o)
+                    observation[1] = closest
+                    closest.points.add(o)
+                    reassignments++
+                }
+            })
+        }
+        do {
+            iter++
+            reassignments = 0
+
+            //Assignment step
+            assign()
+
+            //Update step
+            this.routers.forEach(r => {
+                //Take the mean of all of the router's points, and assign it as the new position
+                const sum = Array.from(r.points).reduce((accumulator, p) => accumulator.add(p), new THREE.Vector3())
+                const mean = sum.divideScalar(r.points.size)
+
+                r.setPosition(mean, true)
+            })
+        } while (iter < 10 && reassignments > 0)
+
+        // Snap each router to its nearest assigned node
+        this.routers.forEach((r) => {
+            const p = r.position
+            const closest = Array.from(r.points).reduce((best, current) => {
+                if (current.distanceTo(p) < best.distanceTo(p)) {
+                    return current
+                }
+                return best
+            })
+            r.position = closest
+        })
+
+        //Assign one more time, because positions have changed
+        assign()
+
+        console.log(`Solved with an average of ${this.getScore()}Mbps`)
+    }
+
+    getScore(): number {
+        let pointCount = 0
+        const routersArray = Array.from(this.routers)
+
+        const sumOfStrengths = routersArray.reduce((acc1, r) => {
+            pointCount += r.points.size
+            return acc1 + Array.from(r.points).reduce((acc2, p) => (acc2 + r.getStrength(p)), 0)
+        }, 0)
+
+        const averageStrength = sumOfStrengths / pointCount
+
+        return averageStrength
+    }
+}
+
+export class Floor extends Node {
+    height: number
+    name = 'Floor'
+
+    constructor({ key, height = 0 }: { height?: number, key?: string } = {}) {
+        super({ key })
         this.height = height
     }
 
-    generateSnapshot(parent: Model) {
-        const out = new Floor(parent)
-        this.children.forEach(c => {
-            const snapshotNode = c.generateSnapshot(out)
-            out.children.add(snapshotNode)
-            snapshotNode.parent = out
-        })
-        return out
+    getArgs(): { [key: string]: unknown } {
+        return { height: this.height, key: this.key }
     }
 
     render() {
         return (
             <>
-                <gridHelper position={new THREE.Vector3(0, this.height, 0)} />
+                <gridHelper key={this.key} position={new THREE.Vector3(0, this.height, 0)} />
                 {Array.from(this.children).map(c => c.render())}
             </>
         )
     }
 }
 
-export class Room extends ModelNode {
-    parent: Floor | null
+export class Room extends Node {
     position: THREE.Vector3
     size: THREE.Vector3
+    name = 'Room'
 
-    constructor(parent: Floor | null, px = 0, pz = 0, sx = 1, sy = 1, sz = 1) {
-        super(parent)
-        this.parent = parent
-        this.position = new THREE.Vector3(px, (this.parent?.height ?? 0) + sy / 2, pz)
-        this.size = new THREE.Vector3(sx, sy, sz)
+    constructor({ key, position = [0, 0, 0], size = [1, 1, 1] }: { key?: string, position?: THREE.Vector3Tuple, size?: THREE.Vector3Tuple } = {}) {
+        super({ key })
+        this.position = new THREE.Vector3(...position)
+        this.size = new THREE.Vector3(...size)
+    }
+
+    getArgs() {
+        return { key: this.key, position: this.position.toArray(), size: this.size.toArray() }
+    }
+
+    getPointCloud(): THREE.Vector3[] {
+        const out: THREE.Vector3[] = []
+
+        for (let x = 0; x < this.size.x; x++) {
+            // Don't optimize for points more than 2 above the ground,
+            // because there usually aren't computers that high.
+            for (let y = 0; y < this.size.y && y <= 2; y++) {
+                for (let z = 0; z < this.size.z; z++) {
+                    out.push(this.position.clone().add(new THREE.Vector3(x, y, z)))
+                }
+            }
+        }
+
+        return out
     }
 
     render() {
+        const position = new THREE.Vector3().copy(this.position)
+
+        const height = (this?.parent as Floor)?.height
+        if (typeof height === 'number') {
+            position.setY(height + this.size.y / 2)
+        }
+
+        const color = this.selected ? 'lightblue' : 'white'
         return (
-            <mesh material={new THREE.MeshPhongMaterial({ color: 'white', flatShading: true })} position={this.position}>
-                <boxGeometry />
-            </mesh>
+            <>
+                <mesh key={this.key} material={new THREE.MeshPhongMaterial({ color, flatShading: true, transparent: true })} position={position}>
+                    <boxGeometry args={this.size.toArray()} />
+                    {Array.from(this.children).map(c => c.render())}
+                </mesh>
+            </>
         )
     }
+}
+
+export class Router extends Node {
+    name = 'Router'
+    position: THREE.Vector3
+    points = new Set<THREE.Vector3>()
+    strength = 1
+    halfDistance = 5 //The amount of units away it takes for the signal strength to decrease by half
+
+    constructor({ key, position = [0, 0, 0] }: { key?: string, position?: THREE.Vector3Tuple } = {}) {
+        super({ key })
+        this.position = new THREE.Vector3(...position)
+    }
+
+    getStrength(at: THREE.Vector3) {
+        const distance = this.position.distanceTo(at)
+
+        return this.strength * (1 / 2) ** (distance / this.halfDistance)
+    }
+
+    setPosition(p: THREE.Vector3, silent=false) {
+        this.position = p.clone()
+        if (!silent) this.onHeirarchyChange()
+    }
+}
+
+const fromJSONDictionary: { [key: string]: (...args: any[]) => Node } = {
+    "Node": (...args) => new Node(...args),
+    "Model": (...args) => new Model(...args),
+    "Floor": (...args) => new Floor(...args),
+    "Room": (...args) => new Room(...args),
+    "Router": (...args) => new Router(...args)
 }
